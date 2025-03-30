13d8f97ed85fb806bc047e6ca377e7a01dc1af09
Lennart Antonius Bergmann <lennart.bergmann05@gmail.com>
2025-03-30 19:35:44 +0200

    Update switcher.sh
    made time between page switch longer and less time between reloads

--- scripts/switcher.sh
+++ scripts/switcher.sh
@@ -3,20 +3,20 @@
 # Anzahl der URLs auslesen
 URLS=$(jq -r '.urls | length' /opt/piosk/config.json)
 
-# Tabs wechseln und alle 10 Durchläufe aktualisieren
-for ((TURN=1; TURN<=$((10*URLS)); TURN++)); do
+# Tabs wechseln und alle 3 Durchläufe aktualisieren
+for ((TURN=1; TURN<=$((3*URLS)); TURN++)); do
   # Tab wechseln (Strg + Tab)
   xdotool key ctrl+Tab
   
   # In den letzten Runden aktualisieren (Strg + R)
-  if [ $TURN -gt $((9*URLS)) ]; then
+  if [ $TURN -gt $((2*URLS)) ]; then
     xdotool key ctrl+r
     
-    if [ $TURN -eq $((10*URLS)) ]; then
+    if [ $TURN -eq $((3*URLS)) ]; then
       (( TURN=0 ))  # Zurücksetzen auf 0, Schleife beginnt von vorne
     fi
   fi
   
-  # Alle 10s zum nächsten Tab
-  sleep 10
+  # Alle 30s zum nächsten Tab
+  sleep 30
 done
